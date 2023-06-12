52a85ac7ced73893b8cf605dd22fb90948a572d6
Hudson de Jesus Ferreira Junior <hudson.junior@MATRIZ.GRUPOADRIANOCOBUCCIO.LOCAL>
2023-06-12 10:07:32 -0300

    Aula useEffect

--- src/App.js
+++ src/App.js
@@ -1,9 +1,59 @@
-function App() {
-  return (
+import { useState, useEffect } from 'react';
+
+function App(){
+  const [input, setInput] = useState('');
+  const [tarefas, setTarefas] = useState([
+    'Pagar a conta de luz',
+    'Estudar React JS'
+  ]);
+
+  useEffect (() => {
+    const tarefasStorage = localStorage.getItem('@tarefas');
+
+    if(tarefasStorage) {
+      setTarefas(JSON.parse(tarefasStorage))
+    }
+    
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('@tarefas', JSON.stringify(tarefas))
+  }, [tarefas]);
+
+
+  function handleRegister(e){
+    e.preventDefault();
+
+    setTarefas([...tarefas, input]);
+    setInput('');
+
+  }
+
+  return(
     <div>
-      <h1>bem vindo</h1>
+      <h1>Cadastrando Tarefas</h1>
+
+      <form onSubmit={handleRegister}>
+        <label>Nome da tarefa:</label><br/>
+        <input 
+         placeholder="Digite uma tarefa" 
+         value={input}
+         onChange={ (e) => setInput(e.target.value) }
+        /><br/>
+        <button type="submit">Registrar</button>
+      </form>
+
+      <br/><br/>
+
+      <ul>
+        {tarefas.map( tarefa => (
+          <li key={tarefa}>{tarefa}</li>
+        ))}
+      </ul>
+
     </div>
   );
 }
 
 export default App;
+
